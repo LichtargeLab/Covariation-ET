06ab1065f12abbda7795076dbd1f781ac2e9a9f8
Daniel <konecki@bcm.edu>
2019-02-22 20:26:00 -0600

    Updating Documentation and Testing: ETMIPC
    Moved the test for pool_init_write_score. The documentation and code for this method did not need to change. The test has not been run yet.

--- src/SupportingClasses/test_ETMIPC.py
+++ src/SupportingClasses/test_ETMIPC.py
@@ -1637,6 +1637,9 @@ class TestETMIPC(TestCase):
     #     # del (full_aln)
     #     # del (assignment_dict)
 
+########################################################################################################################
+########################################################################################################################
+
     def test_calculate_score_and_coverage(self):
         aa_list = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y',
                    '-']
@@ -1695,8 +1698,6 @@ class TestETMIPC(TestCase):
         os.remove(os.path.join(os.path.abspath('../Test/'), 'X.npz'))
         rmtree(os.path.join(out_dir, 'joblib'))
 
-########################################################################################################################
-
     # Could not properly test this method, not sure how to check the global variables in another module like this
     # explicitly, will try to figure it out later. For now the next tests will evaluate if this works or not by proxy.
     # def test_pool_init_write_score(self):
@@ -1740,6 +1741,8 @@ class TestETMIPC(TestCase):
     #         rmtree(os.path.join(out_dir, str(k)))
     #     # del (full_aln)
     #     # del (assignment_dict)
+
+########################################################################################################################
 
     def test_write_score(self):
         aa_list = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y',
