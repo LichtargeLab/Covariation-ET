2d629d6522af7d2e69feb0abb18b22a0d7226248
Daniel <konecki@bcm.edu>
2019-07-11 20:40:40 -0500

    Working on Trace class and testing.
    Added a function to perform the characterization of the groups at each rank. This is single processed, I would like to make it multiprocessed (tree traversal should be optimized for that though). Current tests on the smaller alignment show that this is working as expected at the moment. I have also tested the initialization function.

--- src/SupportingClasses/Trace.py
+++ src/SupportingClasses/Trace.py
@@ -3,27 +3,96 @@ Created on May 23, 2019
 
 @author: Daniel Konecki
 """
-import os
+from time import sleep
+from Queue import Queue
 
 
 class Trace(object):
+    """
+    This class represents the fundamental behavior of the Evolutionary Trace algorithm developed by Olivier Lichtarge
+    and expanded upon by members of his laboratory.
 
-    def __init__(self, aln, phylo_tree, position_specific=True, pair_specific=True, output_dir=None):
-        """
+    Attributes:
+        aln (SeqAlignment): The alignment for which a trace is being performed
+        phylo_tree (PhylogeneticTree): The tree constructed from the alignment over which the trace is performed.
+        assignments (dict): The rank and group assignments made based on the tree.
+        position_specific (bool): Whether this trace will perform position specific analyses or not.
+        pair_specific (bool): Whether this trace will perform pair specific analyses or not.
+    """
 
-        :param aln:
-        :param phylo_tree:
-        :param position_specific:
-        :param pair_specific:
+    def __init__(self, alignment, phylo_tree, group_assignments, position_specific=True, pair_specific=True):
         """
-        self.aln = aln
-        self.tree = phylo_tree
+        Initializer for Trace object.
+
+        Args:
+            alignment (SeqAlignment): The alignment for which to perform a trace analysis.
+            phylo_tree (PhylogeneticTree): The tree based on the alignment to use during the trace analysis.
+            group_assignments (dict): The group assignments for nodes in the tree.
+            position_specific (bool): Whether or not to perform the trace for specific positions.
+            pair_specific (bool): Whether or not to perform the trace for pairs of positions.
+        """
+        self.aln = alignment
+        self.phylo_tree = phylo_tree
+        self.assignments = group_assignments
         self.pos_specific = position_specific
         self.pair_specific = pair_specific
-        self.unique_nodes = {}
-        self.groups = {}
-        self.final = {}
-        if output_dir is None:
-            self.out_dir = os.getcwd()
-        else:
-            self.out_dir = os.path.abspath(output_dir)
+
+    def characterize_rank_groups(self):
+        from time import time
+        start = time()
+        queue = Queue(maxsize=(self.aln.size * 2) - 1)
+        for t in self.phylo_tree.tree.get_terminals():
+            queue.put_nowait(t)
+        for n in self.phylo_tree.traverse_bottom_up():
+            if not n.is_terminal():
+                queue.put_nowait(n)
+        frequency_tables = {}
+        queue_time = time()
+        print('Queue initialized in: {} min'.format((queue_time - start) / 60.0))
+        while queue.qsize() > 0:
+            while1_start = time()
+            node = queue.get_nowait()
+            print('Processing node: {} min'.format(node.name))
+            if node.is_terminal():
+                print('Is Terminal')
+                sub_aln = self.aln.generate_sub_alignment(sequence_ids=[node.name])
+                pos_table, pair_table = sub_aln.characterize_positions(single=self.pos_specific,
+                                                                       pair=self.pair_specific)
+            else:
+                print('Is Non-terminal')
+                child1 = node.clades[0].name
+                child2 = node.clades[1].name
+                print('Children: {}, {}'.format(child1, child2))
+                success = False
+                component1 = None
+                component2 = None
+                while not success:
+                    while2_start = time()
+                    try:
+                        component1 = frequency_tables[child1]
+                        component2 = frequency_tables[child2]
+                        success = True
+                    except KeyError:
+                        sleep(0.5)
+                        exit()
+                    while2_end = time()
+                    print('Inner while took: {} min'.format((while2_end - while2_start) / 60.0))
+                if self.pos_specific:
+                    pos_table = component1['single'] + component2['single']
+                else:
+                    pos_table = None
+                if self.pair_specific:
+                    pair_table = component1['pair'] + component2['pair']
+                else:
+                    pair_table = None
+            component = {'single': pos_table, 'pair': pair_table}
+            frequency_tables[node.name] = component
+            while1_end = time()
+            print('Outer while loop took: {} min'.format((while1_end - while1_start) / 60.0))
+        for rank in self.assignments:
+            for group in self.assignments[rank]:
+                self.assignments[rank][group].update(frequency_tables[self.assignments[rank][group]['node'].name])
+
+    # def single_position_trace(self):
+    #
+    # def pair_position_trace(self):
---
+++ src/SupportingClasses/test_Trace.py
@@ -0,0 +1,119 @@
+"""
+Created on July 11, 2019
+
+@author: Daniel Konecki
+"""
+from Trace import Trace
+from test_Base import TestBase
+from SeqAlignment import SeqAlignment
+from PhylogeneticTree import PhylogeneticTree
+from AlignmentDistanceCalculator import AlignmentDistanceCalculator
+
+
+class TestTrace(TestBase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestTrace, cls).setUpClass()
+        cls.query_aln_fa_small = SeqAlignment(
+            file_name=cls.data_set.protein_data[cls.small_structure_id]['Final_FA_Aln'],
+            query_id=cls.small_structure_id)
+        cls.query_aln_fa_small.import_alignment()
+        cls.phylo_tree_small = PhylogeneticTree()
+        calc = AlignmentDistanceCalculator()
+        cls.phylo_tree_small.construct_tree(dm=calc.get_distance(cls.query_aln_fa_small.alignment))
+        cls.assignments_small = cls.phylo_tree_small.assign_group_rank()
+        cls.query_aln_fa_large = SeqAlignment(
+            file_name=cls.data_set.protein_data[cls.large_structure_id]['Final_FA_Aln'],
+            query_id=cls.large_structure_id)
+        cls.query_aln_fa_large.import_alignment()
+        cls.phylo_tree_large = PhylogeneticTree()
+        calc = AlignmentDistanceCalculator()
+        cls.phylo_tree_large.construct_tree(dm=calc.get_distance(cls.query_aln_fa_large.alignment))
+        cls.assignments_large = cls.phylo_tree_large.assign_group_rank()
+
+    def test1a_init(self):
+        trace_small = Trace(alignment=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
+                            group_assignments=self.assignments_small, position_specific=True, pair_specific=True)
+        self.assertEqual(trace_small.aln.file_name, self.query_aln_fa_small.file_name)
+        self.assertEqual(trace_small.aln.query_id, self.query_aln_fa_small.query_id)
+        for s in range(trace_small.aln.size):
+            self.assertEqual(str(trace_small.aln.alignment[s].seq), str(self.query_aln_fa_small.alignment[s].seq))
+        self.assertEqual(trace_small.aln.seq_order, self.query_aln_fa_small.seq_order)
+        self.assertEqual(str(trace_small.aln.query_sequence), str(self.query_aln_fa_small.query_sequence))
+        self.assertEqual(trace_small.aln.seq_length, self.query_aln_fa_small.seq_length)
+        self.assertEqual(trace_small.aln.size, self.query_aln_fa_small.size)
+        self.assertEqual(trace_small.aln.marked, self.query_aln_fa_small.marked)
+        self.assertEqual(trace_small.aln.polymer_type, self.query_aln_fa_small.polymer_type)
+        self.assertTrue(isinstance(trace_small.aln.alphabet, type(self.query_aln_fa_small.alphabet)))
+        self.assertEqual(len(trace_small.aln.alphabet.letters), len(self.query_aln_fa_small.alphabet.letters))
+        for char in trace_small.aln.alphabet.letters:
+            self.assertTrue(char in self.query_aln_fa_small.alphabet.letters)
+        self.assertEqual(trace_small.tree, self.phylo_tree_small)
+        self.assertEqual(trace_small.assignments, self.assignments_small)
+        self.assertEqual(trace_small.pos_specific, True)
+        self.assertEqual(trace_small.pair_specific, True)
+
+    def test1b_init(self):
+        trace_large = Trace(alignment=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
+                            group_assignments=self.assignments_large, position_specific=True, pair_specific=True)
+        self.assertEqual(trace_large.aln.file_name, self.query_aln_fa_large.file_name)
+        self.assertEqual(trace_large.aln.query_id, self.query_aln_fa_large.query_id)
+        for s in range(trace_large.aln.size):
+            self.assertEqual(str(trace_large.aln.alignment[s].seq), str(self.query_aln_fa_large.alignment[s].seq))
+        self.assertEqual(trace_large.aln.seq_order, self.query_aln_fa_large.seq_order)
+        self.assertEqual(str(trace_large.aln.query_sequence), str(self.query_aln_fa_large.query_sequence))
+        self.assertEqual(trace_large.aln.seq_length, self.query_aln_fa_large.seq_length)
+        self.assertEqual(trace_large.aln.size, self.query_aln_fa_large.size)
+        self.assertEqual(trace_large.aln.marked, self.query_aln_fa_large.marked)
+        self.assertEqual(trace_large.aln.polymer_type, self.query_aln_fa_large.polymer_type)
+        self.assertTrue(isinstance(trace_large.aln.alphabet, type(self.query_aln_fa_large.alphabet)))
+        self.assertEqual(len(trace_large.aln.alphabet.letters), len(self.query_aln_fa_large.alphabet.letters))
+        for char in trace_large.aln.alphabet.letters:
+            self.assertTrue(char in self.query_aln_fa_large.alphabet.letters)
+        self.assertEqual(trace_large.tree, self.phylo_tree_large)
+        self.assertEqual(trace_large.assignments, self.assignments_large)
+        self.assertEqual(trace_large.pos_specific, True)
+        self.assertEqual(trace_large.pair_specific, True)
+
+    def test2a_characterize_rank_groups(self):
+        trace_small = Trace(alignment=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
+                            group_assignments=self.assignments_small, position_specific=True, pair_specific=True)
+        trace_small.characterize_rank_groups()
+        for rank in trace_small.assignments:
+            for group in trace_small.assignments[rank]:
+                sub_aln = self.query_aln_fa_small.generate_sub_alignment(
+                    sequence_ids=trace_small.assignments[rank][group]['terminals'])
+                single_table, pair_table = sub_aln.characterize_positions(single=True, pair=True)
+                self.assertTrue('single' in trace_small.assignments[rank][group])
+                self.assertEqual(trace_small.assignments[rank][group]['single'].get_table(), single_table.get_table())
+                self.assertTrue('pair' in trace_small.assignments[rank][group])
+                self.assertEqual(trace_small.assignments[rank][group]['pair'].get_table(), pair_table.get_table())
+
+    def test2b_characterize_rank_groups(self):
+        trace_small = Trace(alignment=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
+                            group_assignments=self.assignments_small, position_specific=True, pair_specific=False)
+        trace_small.characterize_rank_groups()
+        for rank in trace_small.assignments:
+            for group in trace_small.assignments[rank]:
+                sub_aln = self.query_aln_fa_small.generate_sub_alignment(
+                    sequence_ids=trace_small.assignments[rank][group]['terminals'])
+                single_table, pair_table = sub_aln.characterize_positions(single=True, pair=False)
+                self.assertTrue('single' in trace_small.assignments[rank][group])
+                self.assertEqual(trace_small.assignments[rank][group]['single'].get_table(), single_table.get_table())
+                self.assertTrue('pair' in trace_small.assignments[rank][group])
+                self.assertIsNone(trace_small.assignments[rank][group]['pair'])
+
+    def test2c_characterize_rank_groups(self):
+        trace_small = Trace(alignment=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
+                            group_assignments=self.assignments_small, position_specific=False, pair_specific=True)
+        trace_small.characterize_rank_groups()
+        for rank in trace_small.assignments:
+            for group in trace_small.assignments[rank]:
+                sub_aln = self.query_aln_fa_small.generate_sub_alignment(
+                    sequence_ids=trace_small.assignments[rank][group]['terminals'])
+                single_table, pair_table = sub_aln.characterize_positions(single=True, pair=True)
+                self.assertTrue('single' in trace_small.assignments[rank][group])
+                self.assertIsNone(trace_small.assignments[rank][group]['single'])
+                self.assertTrue('pair' in trace_small.assignments[rank][group])
+                self.assertEqual(trace_small.assignments[rank][group]['pair'].get_table(), pair_table.get_table())
