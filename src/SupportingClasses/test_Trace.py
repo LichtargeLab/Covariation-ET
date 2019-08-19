f32b1b4a03c1e4f4f3a22f0725be9ca4eb201ab5
danny <konecki@bcm.edu>
2019-08-18 23:27:36 -0500

    Still seeing errors on test_EvolutionaryTrace (4e), working through test_Trace to see if anything turns up there.
    Next characterize rank group tests (2a-h).

--- src/SupportingClasses/test_Trace.py
+++ src/SupportingClasses/test_Trace.py
@@ -72,59 +72,59 @@ class TestTrace(TestBase):
     #     rmtree(cls.out_small_dir)
     #     rmtree(cls.out_large_dir)
 
-    def test1a_init(self):
-        trace_small = Trace(alignment=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
-                            group_assignments=self.assignments_small, position_specific=True, pair_specific=True,
-                            output_dir=self.out_small_dir, low_memory=False)
-        self.assertEqual(trace_small.aln.file_name, self.query_aln_fa_small.file_name)
-        self.assertEqual(trace_small.aln.query_id, self.query_aln_fa_small.query_id)
-        for s in range(trace_small.aln.size):
-            self.assertEqual(str(trace_small.aln.alignment[s].seq), str(self.query_aln_fa_small.alignment[s].seq))
-        self.assertEqual(trace_small.aln.seq_order, self.query_aln_fa_small.seq_order)
-        self.assertEqual(str(trace_small.aln.query_sequence), str(self.query_aln_fa_small.query_sequence))
-        self.assertEqual(trace_small.aln.seq_length, self.query_aln_fa_small.seq_length)
-        self.assertEqual(trace_small.aln.size, self.query_aln_fa_small.size)
-        self.assertEqual(trace_small.aln.marked, self.query_aln_fa_small.marked)
-        self.assertEqual(trace_small.aln.polymer_type, self.query_aln_fa_small.polymer_type)
-        self.assertTrue(isinstance(trace_small.aln.alphabet, type(self.query_aln_fa_small.alphabet)))
-        self.assertEqual(len(trace_small.aln.alphabet.letters), len(self.query_aln_fa_small.alphabet.letters))
-        for char in trace_small.aln.alphabet.letters:
-            self.assertTrue(char in self.query_aln_fa_small.alphabet.letters)
-        self.assertEqual(trace_small.phylo_tree, self.phylo_tree_small)
-        self.assertEqual(trace_small.assignments, self.assignments_small)
-        self.assertIsNone(trace_small.unique_nodes)
-        self.assertIsNone(trace_small.rank_scores)
-        self.assertEqual(trace_small.pos_specific, True)
-        self.assertEqual(trace_small.pair_specific, True)
-        self.assertEqual(trace_small.out_dir, self.out_small_dir)
-        self.assertFalse(trace_small.low_memory)
-
-    def test1b_init(self):
-        trace_large = Trace(alignment=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
-                            group_assignments=self.assignments_large, position_specific=True, pair_specific=True,
-                            output_dir=self.out_large_dir, low_memory=True)
-        self.assertEqual(trace_large.aln.file_name, self.query_aln_fa_large.file_name)
-        self.assertEqual(trace_large.aln.query_id, self.query_aln_fa_large.query_id)
-        for s in range(trace_large.aln.size):
-            self.assertEqual(str(trace_large.aln.alignment[s].seq), str(self.query_aln_fa_large.alignment[s].seq))
-        self.assertEqual(trace_large.aln.seq_order, self.query_aln_fa_large.seq_order)
-        self.assertEqual(str(trace_large.aln.query_sequence), str(self.query_aln_fa_large.query_sequence))
-        self.assertEqual(trace_large.aln.seq_length, self.query_aln_fa_large.seq_length)
-        self.assertEqual(trace_large.aln.size, self.query_aln_fa_large.size)
-        self.assertEqual(trace_large.aln.marked, self.query_aln_fa_large.marked)
-        self.assertEqual(trace_large.aln.polymer_type, self.query_aln_fa_large.polymer_type)
-        self.assertTrue(isinstance(trace_large.aln.alphabet, type(self.query_aln_fa_large.alphabet)))
-        self.assertEqual(len(trace_large.aln.alphabet.letters), len(self.query_aln_fa_large.alphabet.letters))
-        for char in trace_large.aln.alphabet.letters:
-            self.assertTrue(char in self.query_aln_fa_large.alphabet.letters)
-        self.assertEqual(trace_large.phylo_tree, self.phylo_tree_large)
-        self.assertEqual(trace_large.assignments, self.assignments_large)
-        self.assertIsNone(trace_large.unique_nodes)
-        self.assertIsNone(trace_large.rank_scores)
-        self.assertEqual(trace_large.pos_specific, True)
-        self.assertEqual(trace_large.pair_specific, True)
-        self.assertEqual(trace_large.out_dir, self.out_large_dir)
-        self.assertTrue(trace_large.low_memory)
+    # def test1a_init(self):
+    #     trace_small = Trace(alignment=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
+    #                         group_assignments=self.assignments_small, position_specific=True, pair_specific=True,
+    #                         output_dir=self.out_small_dir, low_memory=False)
+    #     self.assertEqual(trace_small.aln.file_name, self.query_aln_fa_small.file_name)
+    #     self.assertEqual(trace_small.aln.query_id, self.query_aln_fa_small.query_id)
+    #     for s in range(trace_small.aln.size):
+    #         self.assertEqual(str(trace_small.aln.alignment[s].seq), str(self.query_aln_fa_small.alignment[s].seq))
+    #     self.assertEqual(trace_small.aln.seq_order, self.query_aln_fa_small.seq_order)
+    #     self.assertEqual(str(trace_small.aln.query_sequence), str(self.query_aln_fa_small.query_sequence))
+    #     self.assertEqual(trace_small.aln.seq_length, self.query_aln_fa_small.seq_length)
+    #     self.assertEqual(trace_small.aln.size, self.query_aln_fa_small.size)
+    #     self.assertEqual(trace_small.aln.marked, self.query_aln_fa_small.marked)
+    #     self.assertEqual(trace_small.aln.polymer_type, self.query_aln_fa_small.polymer_type)
+    #     self.assertTrue(isinstance(trace_small.aln.alphabet, type(self.query_aln_fa_small.alphabet)))
+    #     self.assertEqual(len(trace_small.aln.alphabet.letters), len(self.query_aln_fa_small.alphabet.letters))
+    #     for char in trace_small.aln.alphabet.letters:
+    #         self.assertTrue(char in self.query_aln_fa_small.alphabet.letters)
+    #     self.assertEqual(trace_small.phylo_tree, self.phylo_tree_small)
+    #     self.assertEqual(trace_small.assignments, self.assignments_small)
+    #     self.assertIsNone(trace_small.unique_nodes)
+    #     self.assertIsNone(trace_small.rank_scores)
+    #     self.assertEqual(trace_small.pos_specific, True)
+    #     self.assertEqual(trace_small.pair_specific, True)
+    #     self.assertEqual(trace_small.out_dir, self.out_small_dir)
+    #     self.assertFalse(trace_small.low_memory)
+    #
+    # def test1b_init(self):
+    #     trace_large = Trace(alignment=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
+    #                         group_assignments=self.assignments_large, position_specific=True, pair_specific=True,
+    #                         output_dir=self.out_large_dir, low_memory=True)
+    #     self.assertEqual(trace_large.aln.file_name, self.query_aln_fa_large.file_name)
+    #     self.assertEqual(trace_large.aln.query_id, self.query_aln_fa_large.query_id)
+    #     for s in range(trace_large.aln.size):
+    #         self.assertEqual(str(trace_large.aln.alignment[s].seq), str(self.query_aln_fa_large.alignment[s].seq))
+    #     self.assertEqual(trace_large.aln.seq_order, self.query_aln_fa_large.seq_order)
+    #     self.assertEqual(str(trace_large.aln.query_sequence), str(self.query_aln_fa_large.query_sequence))
+    #     self.assertEqual(trace_large.aln.seq_length, self.query_aln_fa_large.seq_length)
+    #     self.assertEqual(trace_large.aln.size, self.query_aln_fa_large.size)
+    #     self.assertEqual(trace_large.aln.marked, self.query_aln_fa_large.marked)
+    #     self.assertEqual(trace_large.aln.polymer_type, self.query_aln_fa_large.polymer_type)
+    #     self.assertTrue(isinstance(trace_large.aln.alphabet, type(self.query_aln_fa_large.alphabet)))
+    #     self.assertEqual(len(trace_large.aln.alphabet.letters), len(self.query_aln_fa_large.alphabet.letters))
+    #     for char in trace_large.aln.alphabet.letters:
+    #         self.assertTrue(char in self.query_aln_fa_large.alphabet.letters)
+    #     self.assertEqual(trace_large.phylo_tree, self.phylo_tree_large)
+    #     self.assertEqual(trace_large.assignments, self.assignments_large)
+    #     self.assertIsNone(trace_large.unique_nodes)
+    #     self.assertIsNone(trace_large.rank_scores)
+    #     self.assertEqual(trace_large.pos_specific, True)
+    #     self.assertEqual(trace_large.pair_specific, True)
+    #     self.assertEqual(trace_large.out_dir, self.out_large_dir)
+    #     self.assertTrue(trace_large.low_memory)
 
     def evaluate_characterize_rank_groups(self, aln, phylo_tree, assign, single, pair, processors, low_mem, write_aln,
                                           write_freq_table):
@@ -183,57 +183,57 @@ class TestTrace(TestBase):
                     visited.add(node_name)
         rmtree(unique_dir)
 
-    # def test2a_characterize_rank_groups(self):
-    #     # Test characterizing both single and pair positions, small alignment, single processed
-    #     self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
-    #                                            assign=self.assignments_small, single=True, pair=True, processors=1,
-    #                                            low_mem=False, write_aln=True, write_freq_table=True)
-    #
-    # def test2b_characterize_rank_groups(self):
-    #     # Test characterizing both single and pair positions, large alignment, single processed
-    #     self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
-    #                                            assign=self.assignments_large, single=True, pair=True, processors=1,
-    #                                            low_mem=True, write_aln=False, write_freq_table=False)
-    #
-    # def test2c_characterize_rank_groups(self):
-    #     # Test characterizing both single and pair positions, small alignment, single processed
-    #     self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
-    #                                            assign=self.assignments_custom_small, single=True, pair=True,
-    #                                            processors=1, low_mem=False, write_aln=True, write_freq_table=True)
-    #
-    # def test2d_characterize_rank_groups(self):
-    #     # Test characterizing both single and pair positions, large alignment, single processed
-    #     self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
-    #                                            assign=self.assignments_custom_large, single=True, pair=True, processors=1,
-    #                                            low_mem=True, write_aln=False, write_freq_table=False)
-    #
-    # def test2e_characterize_rank_groups(self):
-    #     # Test characterizing both single and pair positions, small alignment, multi-processed
-    #     self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
-    #                                            assign=self.assignments_small, single=True, pair=True,
-    #                                            processors=self.max_threads, low_mem=False, write_aln=True,
-    #                                            write_freq_table=True)
-    #
-    # def test2f_characterize_rank_groups(self):
-    #     # Test characterizing both single and pair positions, large alignment, multi-processed
-    #     self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
-    #                                            assign=self.assignments_large, single=True, pair=True,
-    #                                            processors=self.max_threads, low_mem=True, write_aln=False,
-    #                                            write_freq_table=False)
-    #
-    # def test2g_characterize_rank_groups(self):
-    #     # Test characterizing both single and pair positions, small alignment, multi-processed
-    #     self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
-    #                                            assign=self.assignments_custom_small, single=True, pair=True,
-    #                                            processors=self.max_threads, low_mem=False, write_aln=True,
-    #                                            write_freq_table=True)
-    #
-    # def test2h_characterize_rank_groups(self):
-    #     # Test characterizing both single and pair positions, large alignment, multi-processed
-    #     self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
-    #                                            assign=self.assignments_custom_large, single=True, pair=True,
-    #                                            processors=self.max_threads, low_mem=True, write_aln=False,
-    #                                            write_freq_table=False)
+    def test2a_characterize_rank_groups(self):
+        # Test characterizing both single and pair positions, small alignment, single processed
+        self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
+                                               assign=self.assignments_small, single=True, pair=True, processors=1,
+                                               low_mem=False, write_aln=True, write_freq_table=True)
+
+    def test2b_characterize_rank_groups(self):
+        # Test characterizing both single and pair positions, large alignment, single processed
+        self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
+                                               assign=self.assignments_large, single=True, pair=True, processors=1,
+                                               low_mem=True, write_aln=False, write_freq_table=False)
+
+    def test2c_characterize_rank_groups(self):
+        # Test characterizing both single and pair positions, small alignment, single processed
+        self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
+                                               assign=self.assignments_custom_small, single=True, pair=True,
+                                               processors=1, low_mem=False, write_aln=True, write_freq_table=True)
+
+    def test2d_characterize_rank_groups(self):
+        # Test characterizing both single and pair positions, large alignment, single processed
+        self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
+                                               assign=self.assignments_custom_large, single=True, pair=True, processors=1,
+                                               low_mem=True, write_aln=False, write_freq_table=False)
+
+    def test2e_characterize_rank_groups(self):
+        # Test characterizing both single and pair positions, small alignment, multi-processed
+        self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
+                                               assign=self.assignments_small, single=True, pair=True,
+                                               processors=self.max_threads, low_mem=False, write_aln=True,
+                                               write_freq_table=True)
+
+    def test2f_characterize_rank_groups(self):
+        # Test characterizing both single and pair positions, large alignment, multi-processed
+        self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
+                                               assign=self.assignments_large, single=True, pair=True,
+                                               processors=self.max_threads, low_mem=True, write_aln=False,
+                                               write_freq_table=False)
+
+    def test2g_characterize_rank_groups(self):
+        # Test characterizing both single and pair positions, small alignment, multi-processed
+        self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_small, phylo_tree=self.phylo_tree_small,
+                                               assign=self.assignments_custom_small, single=True, pair=True,
+                                               processors=self.max_threads, low_mem=False, write_aln=True,
+                                               write_freq_table=True)
+
+    def test2h_characterize_rank_groups(self):
+        # Test characterizing both single and pair positions, large alignment, multi-processed
+        self.evaluate_characterize_rank_groups(aln=self.query_aln_fa_large, phylo_tree=self.phylo_tree_large,
+                                               assign=self.assignments_custom_large, single=True, pair=True,
+                                               processors=self.max_threads, low_mem=True, write_aln=False,
+                                               write_freq_table=False)
 
     def evaluate_trace(self, aln, phylo_tree, assignments, single, pair, metric, num_proc, out_dir, gap_correction=None,
                        low_mem=True):
