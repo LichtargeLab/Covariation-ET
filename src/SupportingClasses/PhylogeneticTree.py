ed8364e0bc6a81ef6e4f1550d02dacdb57ae3140
Daniel <konecki@bcm.edu>
2019-09-08 08:00:16 -0500

    Switched to the same minimum distance finding approach as upgma tree.

--- src/SupportingClasses/PhylogeneticTree.py
+++ src/SupportingClasses/PhylogeneticTree.py
@@ -309,7 +309,7 @@ class PhylogeneticTree(object):
 
         clades = [BaseTree.Clade(None, name) for name in self.distance_matrix.names]
         inner_count = self.size
-        dm = np.array(self.distance_matrix)
+        dm = np.tril(np.array(self.distance_matrix))
         counts = np.triu(np.ones(dm.shape, dtype=float))
         inner_clade = None
         # old
@@ -321,24 +321,14 @@ class PhylogeneticTree(object):
         dm2 = deepcopy(self.distance_matrix)
         # old
         while dm.shape[0] > 1:
-            min_dist = float(np.min(dm[np.triu_indices(len(dm), k=1)]))
+            min_dist = float(np.min(dm[np.tril_indices(dm.shape[0], k=-1)]))
             positions = np.where(dm == min_dist)
-            upper_triangle_pos = positions[1] > positions[0]
-            min_i = int(positions[0][upper_triangle_pos][0])
-            min_j = int(positions[1][upper_triangle_pos][0])
-            print('MIN DIST: ', min_dist)
-            print('MIN I: ', min_i)
-            print('MIN J: ', min_j)
-            # new?
-            min_dist2 = float(np.min(dm[np.tril_indices(dm.shape[0], k=-1)]))
-            positions2 = np.where(dm == min_dist2)
-            lower_triangle_pos2 = positions2[0] < positions2[1]
-            min_i2 = int(positions2[0][lower_triangle_pos2][-1])
-            min_j2 = int(positions2[1][lower_triangle_pos2][-1])
-            print('MIN DIST2: ', min_dist2)
-            print('MIN I2: ', min_i2)
-            print('MIN J2: ', min_j2)
-            # new?
+            lower_triangle_pos = positions[0] < positions[1]
+            min_i = int(positions[0][lower_triangle_pos][-1])
+            min_j = int(positions[1][lower_triangle_pos][-1])
+            print('MIN DIST2: ', min_dist)
+            print('MIN I2: ', min_i)
+            print('MIN J2: ', min_j)
             clade1 = clades[min_i]
             clade2 = clades[min_j]
             inner_count -= 1
