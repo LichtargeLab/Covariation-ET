ed37ed0dc83dc89d488599d74cc2df5c1663c985
Daniel <konecki@bcm.edu>
2020-02-05 16:57:09 -0600

    Updated DCAWrapper to make use of the Predictor parent class.
    The DCAWrapper now has additional attributes and a different init method (leaning heavily on the Predictor super()). Calculate scores has been updated so that it also computes rankings and coverage values for the predictions since I have made that a part of the base set of attributes (I did this partially so it would not accidentally be done incorrectly later in ContactScorer.py or another context). I have created a set of tests for the DCAWrapper.py which include test for init, import_covariance_score, and calculate_scores.

--- src/SupportingClasses/DCAWrapper.py
+++ src/SupportingClasses/DCAWrapper.py
@@ -14,9 +14,11 @@ try:
 except IOError:
     dotenv_path = find_dotenv(raise_error_if_not_found=True, usecwd=True)
 load_dotenv(dotenv_path)
+from Predictor import Predictor
+from utils import compute_rank_and_coverage
 
 
-class DCAWrapper(object):
+class DCAWrapper(Predictor):
     """
     This class is intended as a wrapper for the DCA julia code distributed through github for the following citation:
         https://github.com/carlobaldassi/GaussDCA.jl
@@ -38,20 +40,20 @@ class DCAWrapper(object):
         covariance scores.
     """
 
-    def __init__(self, alignment):
+    def __init__(self, query, aln_file, out_dir='.'):
         """
-        This method instantiates an instance of the DCAWrapper class.
+        __init__
 
-        Args:
-            alignment (SeqAlignment): This variable holds a SeqAlignment object. This is mainly used to provide the
-            path the to the alignment used, but is also used to determine the length of the query sequence. The DCA code
-            does not focus its analysis on the query sequence and remove gaps, so it is advised that the
-            import_alignment() and remove_gaps() methods be run before the SeqAlignment instance is passed to
-            ETMIPWrapper.
+        The base initialization function for all Predictor sub-classes.
+
+        Arguments:
+            query (str): The sequence identifier for the sequence being analyzed.
+            aln_file (str): The path to the alignment to analyze, the file is expected to be in fasta format.
+            out_dir (str): The path where results of this analysis should be written to. If no path is provided the
+            default will be to write results to the current working directory.
         """
-        self.alignment = alignment
-        self.scores = None
-        self.time = None
+        super().__init__(query, aln_file, out_dir)
+        self.method = 'DCA'
 
     def import_covariance_scores(self, out_path):
         """
@@ -67,14 +69,17 @@ class DCAWrapper(object):
         """
         if not os.path.isfile(out_path):
             raise ValueError('Provided file does not exist: {}!'.format(out_path))
-        data = pd.read_csv(out_path, header=None, sep='\s+', names=['Res_i', 'Res_j' , 'Scores'])
-        self.scores = np.zeros((self.alignment.seq_length, self.alignment.seq_length))
+
+        print('OUTPATH:')
+        print(out_path)
+        data = pd.read_csv(out_path, header=None, sep='\s+', names=['Res_i', 'Res_j', 'Scores'])
+        self.scores = np.zeros((self.non_gapped_aln.seq_length, self.non_gapped_aln.seq_length))
         for ind in data.index:
             i = data.loc[ind, 'Res_i'] - 1
             j = data.loc[ind, 'Res_j'] - 1
             self.scores[i, j] = self.scores[j, i] = data.loc[ind, 'Scores']
 
-    def calculate_scores(self, out_dir, delete_file=True):
+    def calculate_scores(self, delete_file=True):
         """
         Calculate DCA Scores
 
@@ -84,7 +89,6 @@ class DCAWrapper(object):
         import_covariance_scores() to load the data produced by the run.
 
         Args:
-            out_dir (str): The path to the file where the DCA scores should be written.
             delete_file (boolean): If True the file written out by calling this method will be deleted after importing
             the relevant data, if False the file will be left at the specified out_path.
         Returns:
@@ -93,17 +97,19 @@ class DCAWrapper(object):
             ValueError: If the file does not exist, or if the file expected to be created by this method is not
             found in that directory.
         """
-        serialized_path = os.path.join(out_dir, 'DCA.npz')
+        serialized_path = os.path.join(self.out_dir, 'DCA.npz')
         if os.path.isfile(serialized_path):
             loaded_data = np.load(serialized_path)
             self.scores = loaded_data['scores']
+            self.coverages = loaded_data['coverages']
+            self.rankings = loaded_data['rankings']
             self.time = loaded_data['time']
         else:
             julia_path = os.path.join(os.environ.get('PROJECT_PATH'), 'src', 'SupportingClasses', 'cmd_line_GausDCA.jl')
-            out_path = os.path.join(out_dir, 'DCA_predictions.tsv')
+            out_path = os.path.join(self.out_dir, 'DCA_predictions.tsv')
             start = time()
             # Call julia code
-            p = Popen(['julia', julia_path, self.alignment.file_name, out_path], stdout=PIPE, stderr=PIPE)
+            p = Popen(['julia', julia_path, self.non_gapped_aln_fn, out_path], stdout=PIPE, stderr=PIPE)
             # Retrieve communications from julia call
             out, error = p.communicate()
             end = time()
@@ -113,8 +119,11 @@ class DCAWrapper(object):
             print('Error:')
             print(error.decode())
             self.import_covariance_scores(out_path=out_path)
+            self.rankings, self.coverages = compute_rank_and_coverage(seq_length=self.non_gapped_aln.seq_length,
+                                                                      scores=self.scores, pos_size=2, rank_type='max')
             if delete_file:
                 os.remove(out_path)
-            np.savez(serialized_path, scores=self.scores, time=self.time)
+            np.savez(serialized_path, scores=self.scores, coverages=self.coverages, rankings=self.rankings,
+                     time=self.time)
         print(self.time)
         return self.time
