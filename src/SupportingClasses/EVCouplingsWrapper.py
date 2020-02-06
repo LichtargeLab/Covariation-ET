211fc36da62751846b0d013b0a33e2bb6cafeee3
Daniel <konecki@bcm.edu>
2020-02-06 14:58:27 -0600

    Completed updating EVCouplingsWrapper.py to use the parent Predictor class.
    Documentation and testing are up to date and everything is passing. The notebooks have been updated to correctly call the init and calculate scores functions.

--- src/SupportingClasses/EVCouplingsWrapper.py
+++ src/SupportingClasses/EVCouplingsWrapper.py
@@ -41,38 +41,50 @@ class EVCouplingsWrapper(Predictor):
     This wrapper makes it possible to  perform covariance analysis using the EV Couplings method on a fasta formatted
     alignment and to import the covariance scores from the analysis.
 
+    This class inherits from the Predictor class which means it shares the attributes and initializer of that class,
+    as well as implementing the calculate_scores method.
+
     Attributes:
+        out_dir (str): The path where results of this analysis should be written to.
+        query (str): The sequence identifier for the sequence being analyzed.
+        original_aln (SeqAlignment): A SeqAlignment object representing the alignment originally passed in.
+        original_aln_fn (str): The path to the alignment to analyze.
+        non_gapped_aln (SeqAlignment): SeqAlignment object representing the original alignment with all columns which
+        are gaps in the query sequence removed.
+        non_gapped_aln_fn (str): Path to where the non-gapped alignment is written.
+        method (str): 'EVCouplings' for all instances of this class, since this attribute describes how the covariance
+        scores were computed.
         protocol (str): Which evcouplings pipeline protocol to use ('standard' or 'mean_field' expected).
-        alignment (SeqAlignment): The alignment for the protein of interest, from which all gaps have been removed in
-        the query sequence (performed automatically during initializiation).
-        scores (np.array): The raw covariation scores.
+        scores (np.array): The raw scores calculated for pair of position by EVCouplings.
         probability (np.array): The probability based on the raw score that two positions are covarying.
-        time (float): The time in seconds required to perform the predictions.
+        coverages (np.array): The percentage of scores at or better than the score for this pair of position (i.e. the
+        percentile rank).
+        rankings (np.array): The rank (lowest being best, highest being worst) of each pair of positions in the provided
+        alignment as determined from the calculated scores.
+        time (float): The time (in seconds) required to complete the computation of covariance scores by EVCouplings.
     """
 
     def __init__(self, query, aln_file, protocol, out_dir='.'):
+        """
+        __init__
+
+        The initialization function for the EVCouplingsWrapper class which draws on its parent (Predictor)
+        initialization.
+
+        Arguments:
+            query (str): The sequence identifier for the sequence being analyzed.
+            aln_file (str): The path to the alignment to analyze, the file is expected to be in fasta format.
+            protocol (str): Which method ('standard' or 'mean_field') to apply when performing the EVCouplings
+            computation.
+            out_dir (str): The path where results of this analysis should be written to. If no path is provided the
+            default will be to write results to the current working directory.
+        """
         super().__init__(query, aln_file, out_dir)
         self.method = 'EVCouplings'
         self.protocol = protocol
         self.probability = None
 
-    # def __init__(self, alignment, protocol):
-    #     """
-    #     __init__
-    #
-    #     This method instantiates the wrapper for EVCouplings.
-    #
-    #     Args:
-    #         alignment (SeqAlignment): The alignment for the protein of interest.
-    #         protocol (str): Which evcouplings pipeline protocol to use ('standard' or 'mean_field' expected).
-    #     """
-    #     self.protocol = protocol
-    #     self.alignment = alignment.remove_gaps()
-    #     self.scores = None
-    #     self.probability = None
-    #     self.time = None
-
-    def configure_run(self, out_dir, cores):
+    def configure_run(self, cores):
         """
         Configure Run
 
@@ -85,8 +97,6 @@ class EVCouplingsWrapper(Predictor):
         written.
 
         Args:
-            out_dir (str): The directory to which intermediate data and final results (including serialized files)
-            should be written.
             cores (int): The number of processors available to perform this analysis.
         Returns:
             dict: Configurations data structure created by evcouplings package.
@@ -94,11 +104,11 @@ class EVCouplingsWrapper(Predictor):
         sample_config = os.environ.get('EVCOUPLINGS_CONFIG')
         config = read_config_file(sample_config)
         # Globals
-        config["global"]["prefix"] = out_dir + ('/' if not out_dir.endswith('/') else '')
-        config["global"]["sequence_id"] = self.alignment.query_id
-        seq_fn = os.path.join(out_dir, '{}_seq.fasta'.format(self.alignment.query_id))
+        config["global"]["prefix"] = self.out_dir + ('/' if not self.out_dir.endswith('/') else '')
+        config["global"]["sequence_id"] = self.non_gapped_aln.query_id
+        seq_fn = os.path.join(self.out_dir, '{}_seq.fasta'.format(self.non_gapped_aln.query_id))
         with open(seq_fn, 'w') as seq_handle:
-            seq = self.alignment.alignment[self.alignment.seq_order.index(self.alignment.query_id)]
+            seq = self.non_gapped_aln.alignment[self.non_gapped_aln.seq_order.index(self.non_gapped_aln.query_id)]
             write(seq, seq_handle, 'fasta')
         config["global"]["sequence_file"] = seq_fn
         config["global"]["region"] = None
@@ -140,9 +150,9 @@ class EVCouplingsWrapper(Predictor):
         config["stages"] = ['align', 'couplings']
         # Align - only using the
         config["align"]["protocol"] = 'existing'
-        aln_fn = os.path.join(out_dir, '{}_aln.fasta'.format(self.alignment.query_id))
-        self.alignment.write_out_alignment(aln_fn)
-        config["align"]["input_alignment"] = aln_fn
+        # aln_fn = os.path.join(out_dir, '{}_aln.fasta'.format(self.alignment.query_id))
+        # self.alignment.write_out_alignment(aln_fn)
+        config["align"]["input_alignment"] = self.non_gapped_aln_fn
         config["align"]["first_index"] = 1
         config["align"]["compute_num_effective_seqs"] = False
         config["align"]["seqid_filter"] = None
@@ -163,13 +173,13 @@ class EVCouplingsWrapper(Predictor):
             config["couplings"]["pseudo_count"] = 0.5
         else:
             raise AttributeError('Unrecognized EVCouplings Protocol')
-        if isinstance(self.alignment.alphabet.letters, str):
-            alphabet = self.alignment.alphabet.letters
-        elif isinstance(self.alignment.alphabet.letters, list):
-            alphabet = ''.join(self.alignment.alphabet.letters)
+        if isinstance(self.non_gapped_aln.alphabet.letters, str):
+            alphabet = self.non_gapped_aln.alphabet.letters
+        elif isinstance(self.non_gapped_aln.alphabet.letters, list):
+            alphabet = ''.join(self.non_gapped_aln.alphabet.letters)
         else:
             raise TypeError('Alphabet cannot be properly processed when letters are type: {}'.format(
-                self.alignment.alphabet.letters))
+                self.non_gapped_aln.alphabet.letters))
         final_alphabet = '-' + alphabet.replace('-', '')
         config["couplings"]["alphabet"] = final_alphabet
         config["couplings"]["ignore_gaps"] = False
@@ -178,7 +188,7 @@ class EVCouplingsWrapper(Predictor):
         # Compare - skipping
         # Mutate - skipping
         # Fold - skipping
-        write_config_file(os.path.join(out_dir, "{}_config.txt".format(self.alignment.query_id)), config)
+        write_config_file(os.path.join(self.out_dir, "{}_config.txt".format(self.non_gapped_aln.query_id)), config)
         return config
 
     def import_covariance_scores(self, out_path):
@@ -194,16 +204,16 @@ class EVCouplingsWrapper(Predictor):
         """
         if not os.path.isfile(out_path):
             raise ValueError('Provided file does not exist: {}!'.format(out_path))
-        data = pd.read_csv(out_path, sep=',')
-        self.scores = np.zeros((self.alignment.seq_length, self.alignment.seq_length))
-        self.probability = np.zeros((self.alignment.seq_length, self.alignment.seq_length))
+        data = pd.read_csv(out_path, sep=',', dtype={'i': np.int64, 'j': np.int64, 'cn': np.float64,
+                                                     'probability': np.float64})
+        self.scores = np.zeros((self.non_gapped_aln.seq_length, self.non_gapped_aln.seq_length))
+        self.probability = np.zeros((self.non_gapped_aln.seq_length, self.non_gapped_aln.seq_length))
         for ind in data.index:
             i = data.loc[ind, 'i'] - 1
             j = data.loc[ind, 'j'] - 1
             self.scores[i, j] = self.scores[j, i] = data.loc[ind, 'cn']
-            self.probability[i, j] = self.scores[j, i] = data.loc[ind, 'probability']
+            self.probability[i, j] = self.probability[j, i] = data.loc[ind, 'probability']
 
-    # def calculate_scores(self, out_dir, cores=1, delete_files=True):
     def calculate_scores(self, cores=1, delete_files=True):
         """
         Calculate Scores
@@ -213,8 +223,6 @@ class EVCouplingsWrapper(Predictor):
         pipeline for that alignment.
 
         Args:
-            out_dir (str): The directory to which intermediate data and final results (including serialized files)
-            should be written.
             cores (int): The number of processors available to perform this analysis.
             delete_files (bool): Whether or not to delete the data files generated by the EVCouplings pipeline
             (intermediate files created by the wrapper and the serialization file will be kept).
@@ -231,7 +239,7 @@ class EVCouplingsWrapper(Predictor):
             self.rankings = loaded_data['rankings']
             self.time = loaded_data['time']
         else:
-            config = self.configure_run(out_dir=self.out_dir, cores=cores)
+            config = self.configure_run(cores=cores)
             out_path = os.path.join(self.out_dir, 'couplings', '_CouplingScores.csv')
             start = time()
             # Call EVCouplings pipeline
--- src/SupportingClasses/test_EVCouplingsWrapper.py
+++ src/SupportingClasses/test_EVCouplingsWrapper.py
@@ -9,7 +9,13 @@ import numpy as np
 from time import time
 from shutil import rmtree
 from multiprocessing import cpu_count
+from dotenv import find_dotenv, load_dotenv
 from evcouplings.utils import read_config_file
+try:
+    dotenv_path = find_dotenv(raise_error_if_not_found=True)
+except IOError:
+    dotenv_path = find_dotenv(raise_error_if_not_found=True, usecwd=True)
+load_dotenv(dotenv_path)
 from test_Base import TestBase
 from EVCouplingsWrapper import EVCouplingsWrapper
 from utils import compute_rank_and_coverage
@@ -66,7 +72,7 @@ class TestEVCouplingsWrapper(TestBase):
             rmtree(out_dir)
         evc = EVCouplingsWrapper(query=query, aln_file=aln_file, out_dir=out_dir, protocol='standard')
         expected_cpus = cpu_count()
-        evc.configure_run(out_dir=out_dir, cores=expected_cpus)
+        evc.configure_run(cores=expected_cpus)
         expected_config_fn = os.path.join(out_dir, "{}_config.txt".format(query))
         self.assertTrue(os.path.isfile(expected_config_fn))
         config = read_config_file(expected_config_fn)
@@ -74,76 +80,161 @@ class TestEVCouplingsWrapper(TestBase):
         self.assertEqual(config["global"]["sequence_id"], query)
         self.assertEqual(config["global"]["sequence_file"], os.path.join(out_dir, '{}_seq.fasta'.format(query)))
         self.assertIsNone(config["global"]["region"])
-        self.assertEqual()
+        self.assertEqual(config["global"]["theta"], 0.8)
+        self.assertEqual(config["global"]["cpu"], expected_cpus)
+        self.assertEqual(config["pipeline"], 'protein_monomer')
+        self.assertIsNone(config["batch"])
+        self.assertIsNone(config["management"]["database_uri"])
+        self.assertIsNone(config["management"]["job_name"])
+        self.assertIsNone(config["management"]["archive"])
+        self.assertEqual(config["environment"]["engine"], 'local')
+        self.assertIsNone(config["environment"]["queue"])
+        self.assertEqual(config["environment"]["cores"], expected_cpus)
+        self.assertIsNone(config["environment"]["memory"])
+        self.assertIsNone(config["environment"]["time"])
+        self.assertIsNone(config["environment"]["configuration"])
+        self.assertEqual(config["tools"]["plmc"], os.environ.get('PLMC'))
+        self.assertEqual(config["tools"]["jackhmmer"], os.environ.get('JACKHMMER'))
+        self.assertEqual(config["tools"]["hmmbuild"], os.environ.get('HMMBUILD'))
+        self.assertEqual(config["tools"]["hmmsearch"], os.environ.get('HMMSEARCH'))
+        self.assertIsNone(config["tools"]["hhfilter"])
+        self.assertIsNone(config["tools"]["psipred"])
+        self.assertIsNone(config["tools"]["cns"])
+        self.assertIsNone(config["tools"]["maxcluster"])
+        self.assertEqual(config["databases"]["uniprot"], os.environ.get('UNIPROT'))
+        self.assertEqual(config["databases"]["uniref100"], os.environ.get('UNIREF100'))
+        self.assertEqual(config["databases"]["uniref90"], os.environ.get('UNIREF90'))
+        self.assertIsNone(config["databases"]["sequence_download_url"])
+        self.assertIsNone(config["databases"]["pdb_mmtf_dir"])
+        self.assertEqual(config["databases"]["sifts_mapping_table"], os.environ.get('SIFTS_MAPPING_TABLE'))
+        self.assertEqual(config["databases"]["sifts_sequence_db"], os.environ.get('SIFTS_SEQUENCE_DB'))
+        self.assertEqual(config["stages"], ['align', 'couplings'])
+        self.assertEqual(config["align"]["protocol"], 'existing')
+        self.assertEqual(config["align"]["input_alignment"], evc.non_gapped_aln_fn)
+        self.assertEqual(config["align"]["first_index"], 1)
+        self.assertFalse(config["align"]["compute_num_effective_seqs"])
+        self.assertIsNone(config["align"]["seqid_filter"])
+        self.assertEqual(config["align"]["minimum_sequence_coverage"], 0)
+        self.assertEqual(config["align"]["minimum_column_coverage"], 0)
+        self.assertFalse(config["align"]["extract_annotation"])
+        self.assertEqual(config["couplings"]["protocol"], 'standard')
+        self.assertEqual(config["couplings"]["iterations"], 100)
+        self.assertEqual(config["couplings"]["lambda_J"], 0.01)
+        self.assertTrue(config["couplings"]["lambda_J_times_Lq"])
+        self.assertEqual(config["couplings"]["lambda_h"], 0.01)
+        self.assertIsNone(config["couplings"]["lambda_group"])
+        self.assertIsNone(config["couplings"]["scale_clusters"])
+        if isinstance(evc.non_gapped_aln.alphabet.letters, str):
+            alphabet = evc.non_gapped_aln.alphabet.letters
+        elif isinstance(evc.non_gapped_aln.alphabet.letters, list):
+            alphabet = ''.join(evc.non_gapped_aln.alphabet.letters)
+        else:
+            raise TypeError('Alphabet cannot be properly processed when letters are type: {}'.format(
+                evc.non_gapped_aln.alphabet.letters))
+        expected_alphabet = '-' + alphabet.replace('-', '')
+        self.assertEqual(config["couplings"]["alphabet"], expected_alphabet)
+        self.assertFalse(config["couplings"]["ignore_gaps"])
+        self.assertTrue(config["couplings"]["reuse_ecs"])
+        self.assertEqual(config["couplings"]["min_sequence_distance"], 0)
 
+    def test_2a_configure_run(self):
+        self.evaluate_configure_run(
+            query=self.small_structure_id, aln_file=self.small_fa_fn, out_dir=self.out_small_dir,
+            expected_length=self.data_set.protein_data[self.small_structure_id]['Length'])
 
-    # def evaluate_import_scores(self, query, aln_file, out_dir, expected_length):
-    #     dca = EVCouplingsWrapper(query=query, aln_file=aln_file, out_dir=out_dir)
-    #     scores = np.random.RandomState(1234567890).rand(expected_length, expected_length)
-    #     scores[np.tril_indices(expected_length, 1)] = 0
-    #     scores += scores.T
-    #     indices = np.triu_indices(expected_length, 1)
-    #     sorted_scores, sorted_x, sorted_y = zip(*sorted(zip(scores[indices], indices[0], indices[1])))
-    #     with open(os.path.join(out_dir, 'DCA_predictions.tsv'), 'w') as handle:
-    #         for i in range(len(sorted_scores)):
-    #             handle.write('{} {} {}\n'.format(sorted_x[i] + 1, sorted_y[i] + 1, sorted_scores[i]))
-    #     dca.import_covariance_scores(out_path=os.path.join(out_dir, 'DCA_predictions.tsv'))
-    #     diff_scores = dca.scores - scores
-    #     not_passing_scores = diff_scores > 1E15
-    #     self.assertFalse(not_passing_scores.any())
-    #
-    # def test_2a_import_scores(self):
-    #     self.evaluate_import_scores(
-    #         query=self.small_structure_id, aln_file=self.small_fa_fn, out_dir=self.out_small_dir,
-    #         expected_length=self.data_set.protein_data[self.small_structure_id]['Length'])
-    #
-    # def test_2b_import_scores(self):
-    #     self.evaluate_import_scores(
-    #         query=self.large_structure_id, aln_file=self.large_fa_fn, out_dir=self.out_large_dir,
-    #         expected_length=self.data_set.protein_data[self.large_structure_id]['Length'])
-    #
-    # def evaluate_calculator_scores(self, query, aln_file, out_dir, expected_length, expected_sequence):
-    #     dca = EVCouplingsWrapper(query=query, aln_file=aln_file, out_dir=out_dir)
-    #     start = time()
-    #     dca.calculate_scores(delete_file=False)
-    #     end = time()
-    #     expected_time = end - start
-    #     self.assertEqual(dca.out_dir, os.path.abspath(out_dir))
-    #     self.assertTrue(os.path.isdir(os.path.abspath(out_dir)))
-    #     self.assertEqual(dca.query, query)
-    #     self.assertIsNotNone(dca.original_aln)
-    #     self.assertGreaterEqual(dca.original_aln.seq_length, expected_length)
-    #     self.assertEqual(str(dca.original_aln.query_sequence).replace('-', ''), expected_sequence)
-    #     self.assertEqual(dca.original_aln_fn, os.path.join(out_dir, 'Original_Alignment.fa'))
-    #     self.assertTrue(os.path.isfile(dca.original_aln_fn))
-    #     self.assertIsNotNone(dca.non_gapped_aln)
-    #     self.assertEqual(dca.non_gapped_aln.seq_length, expected_length)
-    #     self.assertEqual(dca.non_gapped_aln.query_sequence, expected_sequence)
-    #     self.assertEqual(dca.non_gapped_aln_fn, os.path.join(out_dir, 'Non-Gapped_Alignment.fa'))
-    #     self.assertTrue(os.path.isfile(dca.non_gapped_aln_fn))
-    #     self.assertEqual(dca.method, 'DCA')
-    #     self.assertIsNotNone(dca.scores)
-    #     expected_ranks, expected_coverages = compute_rank_and_coverage(expected_length, dca.scores, 2, 'max')
-    #     ranks_diff = dca.rankings - expected_ranks
-    #     ranks_not_passing = ranks_diff > 0.0
-    #     self.assertFalse(ranks_not_passing.any())
-    #     coverages_diff = dca.coverages - expected_coverages
-    #     coverages_not_passing = coverages_diff > 0.0
-    #     self.assertFalse(coverages_not_passing.any())
-    #     self.assertLessEqual(dca.time, expected_time)
-    #     self.assertTrue(os.path.isfile(os.path.join(out_dir, 'DCA.npz')))
-    #
-    # def test_3a_calculate_scores(self):
-    #     self.evaluate_calculator_scores(
-    #         query=self.small_structure_id, aln_file=self.small_fa_fn, out_dir=self.out_small_dir,
-    #         expected_length=self.data_set.protein_data[self.small_structure_id]['Length'],
-    #         expected_sequence=str(self.data_set.protein_data[self.small_structure_id]['Sequence'].seq))
-    #
-    # def test_3b_calculate_scores(self):
-    #     self.evaluate_calculator_scores(
-    #         query=self.large_structure_id, aln_file=self.large_fa_fn, out_dir=self.out_large_dir,
-    #         expected_length=self.data_set.protein_data[self.large_structure_id]['Length'],
-    #         expected_sequence=str(self.data_set.protein_data[self.large_structure_id]['Sequence'].seq))
+    def test_2b_configure_run(self):
+        self.evaluate_configure_run(
+            query=self.large_structure_id, aln_file=self.large_fa_fn, out_dir=self.out_large_dir,
+            expected_length=self.data_set.protein_data[self.large_structure_id]['Length'])
+
+    def evaluate_import_scores(self, query, aln_file, out_dir, expected_length):
+        evc = EVCouplingsWrapper(query=query, aln_file=aln_file, out_dir=out_dir, protocol='standard')
+        scores = np.random.RandomState(1234567890).rand(expected_length, expected_length)
+        scores[np.tril_indices(expected_length, 1)] = 0
+        scores += scores.T
+        _, coverages = compute_rank_and_coverage(expected_length, scores, 2, 'max')
+        probabilities = 1 - coverages
+        indices = np.triu_indices(expected_length, 1)
+        sorted_scores, sorted_x, sorted_y, sorted_probability = zip(*sorted(zip(scores[indices], indices[0], indices[1],
+                                                                                probabilities[indices])))
+        expected_dir = os.path.join(out_dir, 'couplings')
+        os.makedirs(expected_dir, exist_ok=True)
+        expected_path = os.path.join(expected_dir, '_CouplingScores.csv')
+        with open(expected_path, 'w') as handle:
+            handle.write('i,A_i,j,A_j,fn,cn,segment_i,segment_j,probability\n')
+            for i in range(len(sorted_scores)):
+                handle.write('{},X,{},X,0,{},X,X,{}\n'.format(sorted_x[i] + 1, sorted_y[i] + 1, sorted_scores[i],
+                                                              sorted_probability[i]))
+        evc.import_covariance_scores(out_path=expected_path)
+
+        diff_expected_scores = scores - scores.T
+        not_passing_expected_scores = diff_expected_scores > 1E-15
+        self.assertFalse(not_passing_expected_scores.any())
+        diff_computed_scores = evc.scores - evc.scores.T
+        not_passing_computed_scores = diff_computed_scores > 1E-15
+        self.assertFalse(not_passing_computed_scores.any())
+
+        diff_scores = evc.scores - scores
+        not_passing_scores = diff_scores > 1E-15
+        self.assertFalse(not_passing_scores.any())
+        diff_probabilities = evc.probability - probabilities
+        not_passing_protbabilities = diff_probabilities > 1E-15
+        self.assertFalse(not_passing_protbabilities.any())
+
+    def test_3a_import_scores(self):
+        self.evaluate_import_scores(
+            query=self.small_structure_id, aln_file=self.small_fa_fn, out_dir=self.out_small_dir,
+            expected_length=self.data_set.protein_data[self.small_structure_id]['Length'])
+
+    def test_3b_import_scores(self):
+        self.evaluate_import_scores(
+            query=self.large_structure_id, aln_file=self.large_fa_fn, out_dir=self.out_large_dir,
+            expected_length=self.data_set.protein_data[self.large_structure_id]['Length'])
+
+    def evaluate_calculator_scores(self, query, aln_file, out_dir, expected_length, expected_sequence):
+        evc = EVCouplingsWrapper(query=query, aln_file=aln_file, out_dir=out_dir, protocol='standard')
+        start = time()
+        evc.calculate_scores(delete_files=False, cores=self.max_threads)
+        end = time()
+        expected_time = end - start
+        self.assertEqual(evc.out_dir, os.path.abspath(out_dir))
+        self.assertTrue(os.path.isdir(os.path.abspath(out_dir)))
+        self.assertEqual(evc.query, query)
+        self.assertIsNotNone(evc.original_aln)
+        self.assertGreaterEqual(evc.original_aln.seq_length, expected_length)
+        self.assertEqual(str(evc.original_aln.query_sequence).replace('-', ''), expected_sequence)
+        self.assertEqual(evc.original_aln_fn, os.path.join(out_dir, 'Original_Alignment.fa'))
+        self.assertTrue(os.path.isfile(evc.original_aln_fn))
+        self.assertIsNotNone(evc.non_gapped_aln)
+        self.assertEqual(evc.non_gapped_aln.seq_length, expected_length)
+        self.assertEqual(evc.non_gapped_aln.query_sequence, expected_sequence)
+        self.assertEqual(evc.non_gapped_aln_fn, os.path.join(out_dir, 'Non-Gapped_Alignment.fa'))
+        self.assertTrue(os.path.isfile(evc.non_gapped_aln_fn))
+        self.assertEqual(evc.method, 'EVCouplings')
+        self.assertEqual(evc.protocol, 'standard')
+        self.assertIsNotNone(evc.scores)
+        self.assertIsNotNone(evc.probability)
+        expected_ranks, expected_coverages = compute_rank_and_coverage(expected_length, evc.scores, 2, 'max')
+        ranks_diff = evc.rankings - expected_ranks
+        ranks_not_passing = ranks_diff > 0.0
+        self.assertFalse(ranks_not_passing.any())
+        coverages_diff = evc.coverages - expected_coverages
+        coverages_not_passing = coverages_diff > 0.0
+        self.assertFalse(coverages_not_passing.any())
+        self.assertLessEqual(evc.time, expected_time)
+        self.assertTrue(os.path.isfile(os.path.join(out_dir, 'EVCouplings.npz')))
+
+    def test_4a_calculate_scores(self):
+        self.evaluate_calculator_scores(
+            query=self.small_structure_id, aln_file=self.small_fa_fn, out_dir=self.out_small_dir,
+            expected_length=self.data_set.protein_data[self.small_structure_id]['Length'],
+            expected_sequence=str(self.data_set.protein_data[self.small_structure_id]['Sequence'].seq))
+
+    def test_4b_calculate_scores(self):
+        self.evaluate_calculator_scores(
+            query=self.large_structure_id, aln_file=self.large_fa_fn, out_dir=self.out_large_dir,
+            expected_length=self.data_set.protein_data[self.large_structure_id]['Length'],
+            expected_sequence=str(self.data_set.protein_data[self.large_structure_id]['Sequence'].seq))
 
 
 if __name__ == '__main__':
