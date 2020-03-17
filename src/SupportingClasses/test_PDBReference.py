c4528e30fd9ed60f34eed2aac9f42ca299723d48
danny <konecki@bcm.edu>
2020-03-17 18:24:33 -0500

    Updated test_PDBReference.py to reduce duplication and speed up testing. All tests are still passing.

--- src/SupportingClasses/test_PDBReference.py
+++ src/SupportingClasses/test_PDBReference.py
@@ -17,43 +17,46 @@ class TestPDBReference(TestBase):
                                 'ESYFGAKAKIDCSSGTLSDVALYFYVRGRDTYVITDALSTGSCSGDVEYPTK'
         cls.expected2_len = 222
 
-    def test1__init__(self):
+    def evaluate__init__(self, pdb_file):
         with self.assertRaises(TypeError):
             PDBReference()
-        pdb1 = PDBReference(pdb_file=self.data_set.protein_data[self.small_structure_id]['PDB'])
-        self.assertEqual(pdb1.file_name, self.data_set.protein_data[self.small_structure_id]['PDB'])
-        self.assertIsNone(pdb1.structure)
-        self.assertIsNone(pdb1.chains)
-        self.assertIsNone(pdb1.seq)
-        self.assertIsNone(pdb1.pdb_residue_list)
-        self.assertIsNone(pdb1.residue_pos)
-        self.assertIsNone(pdb1.size)
+        pdb = PDBReference(pdb_file=pdb_file)
+        self.assertEqual(pdb.file_name, pdb_file)
+        self.assertIsNone(pdb.structure)
+        self.assertIsNone(pdb.chains)
+        self.assertIsNone(pdb.seq)
+        self.assertIsNone(pdb.pdb_residue_list)
+        self.assertIsNone(pdb.residue_pos)
+        self.assertIsNone(pdb.size)
+
+    def test1a__init__(self):
+        self.evaluate__init__(pdb_file=self.data_set.protein_data[self.small_structure_id]['PDB'])
+
+    def test1b__init__(self):
+        self.evaluate__init__(pdb_file=self.data_set.protein_data[self.large_structure_id]['PDB'])
+
+    def evaluate_import_pdb(self, pdb_file, query, expected_chain, expected_sequence, expected_len):
+        pdb1 = PDBReference(pdb_file=pdb_file)
+        pdb1.import_pdb(structure_id=query)
+        self.assertEqual(pdb1.file_name, pdb_file)
+        self.assertIsInstance(pdb1.structure, Structure)
+        self.assertTrue(expected_chain in pdb1.chains)
+        self.assertEqual(pdb1.seq['A'], expected_sequence)
+        for i in range(1, expected_len + 1):
+            self.assertGreaterEqual(pdb1.pdb_residue_list[expected_chain][i - 1], i)
+        expected_dict = {pdb1.pdb_residue_list[expected_chain][i]: expected_sequence[i] for i in range(expected_len)}
+        self.assertEqual(pdb1.residue_pos[expected_chain], expected_dict)
+        self.assertEqual(pdb1.size[expected_chain], expected_len)
 
     def test2a_import_pdb(self):
-        pdb1 = PDBReference(pdb_file=self.data_set.protein_data[self.small_structure_id]['PDB'])
-        pdb1.import_pdb(structure_id=self.small_structure_id)
-        self.assertEqual(pdb1.file_name, self.data_set.protein_data[self.small_structure_id]['PDB'])
-        self.assertIsInstance(pdb1.structure, Structure)
-        self.assertTrue('A' in pdb1.chains)
-        self.assertEqual(pdb1.seq['A'], self.expected1_chain_a)
-        for i in range(1, self.expected1_len + 1):
-            self.assertGreaterEqual(pdb1.pdb_residue_list['A'][i - 1], i)
-        expected_dict = {pdb1.pdb_residue_list['A'][i]: self.expected1_chain_a[i] for i in range(self.expected1_len)}
-        self.assertEqual(pdb1.residue_pos['A'], expected_dict)
-        self.assertEqual(pdb1.size['A'], self.expected1_len)
+        self.evaluate_import_pdb(pdb_file=self.data_set.protein_data[self.small_structure_id]['PDB'],
+                                 query=self.small_structure_id, expected_chain='A',
+                                 expected_sequence=self.expected1_chain_a, expected_len=self.expected1_len)
 
     def test2b_import_pdb(self):
-        pdb2 = PDBReference(pdb_file=self.data_set.protein_data[self.large_structure_id]['PDB'])
-        pdb2.import_pdb(structure_id=self.large_structure_id)
-        self.assertEqual(pdb2.file_name, self.data_set.protein_data[self.large_structure_id]['PDB'])
-        self.assertIsInstance(pdb2.structure, Structure)
-        self.assertTrue('A' in pdb2.chains)
-        self.assertEqual(pdb2.seq['A'], self.expected2_chain_a)
-        for i in range(1, self.expected2_len + 1):
-            self.assertGreaterEqual(pdb2.pdb_residue_list['A'][i - 1], i)
-        expected_dict = {pdb2.pdb_residue_list['A'][i]: self.expected2_chain_a[i] for i in range(self.expected2_len)}
-        self.assertEqual(pdb2.residue_pos['A'], expected_dict)
-        self.assertEqual(pdb2.size['A'], self.expected2_len)
+        self.evaluate_import_pdb(pdb_file=self.data_set.protein_data[self.large_structure_id]['PDB'],
+                                 query=self.large_structure_id, expected_chain='A',
+                                 expected_sequence=self.expected2_chain_a, expected_len=self.expected2_len)
 
 
 if __name__ == '__main__':
