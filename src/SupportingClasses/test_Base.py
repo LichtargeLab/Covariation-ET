0c681d380b2f790499446891ae31e0f17fbf22b9
danny <konecki@bcm.edu>
2020-11-06 20:11:33 -0600

    Reducing duplication across tests.
    Added some of the most used variables across tests to test_Base.py. Significantly reduced duplication in test_seqAlignment.py using the variables in test_Base.py and making evaluate methods for each set of tests which covered all or almost all tests for a function and only require the inputs and expected returns. I also broke the tests down further into more specific classes that match the function they are testing better.

--- src/SupportingClasses/test_Base.py
+++ src/SupportingClasses/test_Base.py
@@ -5,6 +5,10 @@ Created onJune 19, 2019
 """
 import os
 from datetime import datetime
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
+from EvolutionaryTraceAlphabet import FullIUPACDNA, FullIUPACProtein
 from unittest import TestCase
 from multiprocessing import cpu_count
 from DataSetGenerator import DataSetGenerator
@@ -22,37 +26,53 @@ def write_out_temp_fn(suffix, out_str=None):
             handle.write(out_str)
     return fn
 
-class TestBase(TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.max_threads = cpu_count() - 2
-        cls.max_target_seqs = 150
-        cls.testing_dir = os.environ.get('TEST_PATH')
-        cls.input_path = os.path.join(cls.testing_dir, 'Input')
-        cls.protein_list_path = os.path.join(cls.input_path, 'ProteinLists')
-        if not os.path.isdir(cls.protein_list_path):
-            os.makedirs(cls.protein_list_path)
-        cls.small_structure_id = '135l'
-        cls.large_structure_id = '1bol'
-        cls.protein_list_fn = os.path.join(cls.protein_list_path, 'Test_Set.txt')
-        structure_ids = [cls.small_structure_id, cls.large_structure_id]
-        with open(cls.protein_list_fn, 'w') as test_list_handle:
-            for structure_id in structure_ids:
-                test_list_handle.write('{}{}\n'.format(structure_id, 'A'))
-        cls.data_set = DataSetGenerator(input_path=cls.input_path)
-        cls.data_set.build_pdb_alignment_dataset(protein_list_fn='Test_Set.txt', processes=cls.max_threads,
-                                                 max_target_seqs=cls.max_target_seqs)
+# Variables to be used by tests, some of these variables rely on classes which are being tested, only the precursors
+# data to a given class will be used in the tests of that class.
 
-    @classmethod
-    def tearDownClass(cls):
-        # rmtree(cls.input_path)
-        del cls.max_threads
-        del cls.max_target_seqs
-        del cls.testing_dir
-        del cls.input_path
-        del cls.protein_list_path
-        del cls.small_structure_id
-        del cls.large_structure_id
-        del cls.protein_list_fn
-        del cls.data_set
+protein_short_seq = SeqRecord(id='seq1', seq=Seq('MET', alphabet=FullIUPACProtein()))
+protein_seq1 = SeqRecord(id='seq1', seq=Seq('MET---', alphabet=FullIUPACProtein()))
+protein_seq2 = SeqRecord(id='seq2', seq=Seq('M-TREE', alphabet=FullIUPACProtein()))
+protein_seq3 = SeqRecord(id='seq3', seq=Seq('M-FREE', alphabet=FullIUPACProtein()))
+protein_seq4 = SeqRecord(id='seq4', seq=Seq('------', alphabet=FullIUPACProtein()))
+protein_msa = MultipleSeqAlignment(records=[protein_seq1, protein_seq2, protein_seq3], alphabet=FullIUPACProtein())
+dna_seq1 = SeqRecord(id='seq1', seq=Seq('ATGGAGACT---------', alphabet=FullIUPACDNA()))
+dna_seq2 = SeqRecord(id='seq2', seq=Seq('ATG---ACTAGAGAGGAG', alphabet=FullIUPACDNA()))
+dna_seq3 = SeqRecord(id='seq3', seq=Seq('ATG---TTTAGAGAGGAG', alphabet=FullIUPACDNA()))
+dna_msa = MultipleSeqAlignment(records=[dna_seq1, dna_seq2, dna_seq3], alphabet=FullIUPACDNA())
+
+
+# class TestBase(TestCase):
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         cls.max_threads = cpu_count() - 2
+#         cls.max_target_seqs = 150
+#         cls.testing_dir = os.environ.get('TEST_PATH')
+#         cls.input_path = os.path.join(cls.testing_dir, 'Input')
+#         cls.protein_list_path = os.path.join(cls.input_path, 'ProteinLists')
+#         if not os.path.isdir(cls.protein_list_path):
+#             os.makedirs(cls.protein_list_path)
+#         cls.small_structure_id = '135l'
+#         cls.large_structure_id = '1bol'
+#         cls.protein_list_fn = os.path.join(cls.protein_list_path, 'Test_Set.txt')
+#         structure_ids = [cls.small_structure_id, cls.large_structure_id]
+#         with open(cls.protein_list_fn, 'w') as test_list_handle:
+#             for structure_id in structure_ids:
+#                 test_list_handle.write('{}{}\n'.format(structure_id, 'A'))
+#         cls.data_set = DataSetGenerator(input_path=cls.input_path)
+#         cls.data_set.build_pdb_alignment_dataset(protein_list_fn='Test_Set.txt', processes=cls.max_threads,
+#                                                  max_target_seqs=cls.max_target_seqs)
+#
+#     @classmethod
+#     def tearDownClass(cls):
+#         # rmtree(cls.input_path)
+#         del cls.max_threads
+#         del cls.max_target_seqs
+#         del cls.testing_dir
+#         del cls.input_path
+#         del cls.protein_list_path
+#         del cls.small_structure_id
+#         del cls.large_structure_id
+#         del cls.protein_list_fn
+#         del cls.data_set
