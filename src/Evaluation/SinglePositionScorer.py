ce6a2103d9a7adf0e1f4b492ea5b23caeebea897
Daniel <konecki@bcm.edu>
2021-05-22 17:24:57 -0500

    Created new class SinglePositionScorer.py for the scoring of residue importance predictions.
    This class inherits from the Scorer parent class. So far it only includes an __init__ function which passes a small set of tests.

---
+++ src/Evaluation/SinglePositionScorer.py
@@ -0,0 +1,45 @@
+"""
+Created on May 22, 2021
+
+@author: dmkonecki
+"""
+from Evaluation.Scorer import Scorer
+
+
+class SinglePositionScorer(Scorer):
+    """
+    SinglePositionScorer
+
+    This class is meant to abstract the process of scoring a set of residue importance predictions.  This is being done
+    for two main reasons. First, residue importance predictions are being made with several different methods and so
+    their scoring should be performed consistently by another object or function. Second, this class will support
+    several scoring methods such as the hypergoemtric test scoring of overlap with key sites as well as methods internal
+    to the lab like the Selection Cluster Weighting Z-score derived from prior ET work.
+
+    Attributes:
+        query_pdb_mapper (SequencePDBMap): A mapping from the index of the query sequence to the index of the
+        pdb chain's sequence for those positions which match according to a pairwise global alignment and vice versa.
+        data (panda.DataFrame): A data frame containing a mapping of the sequence, structure, and predictions as well as
+        labels to indicate which sequence separation category and top prediction category specific scores belong to.
+        This is used for most evaluation methods.
+    """
+
+    def __init__(self, query, seq_alignment, pdb_reference, chain=None):
+        """
+        __init__
+
+        This function initializes a new ContactScorer, it accepts paths to an alignment and if available a pdb file to
+        be used in the assessment of contact predictions for a given query. It also requires a cutoff which is used to
+        denote which residues are actually in contact, based on their distance to one another.
+
+        Args:
+            query (str): The name of the query sequence/structure.
+            seq_alignment (str/path, SeqAlignment): Path to the alignment being evaluated in this contact scoring
+            prediction task or an already initialized SeqAlignment object.
+            pdb_reference (str/path, PDBReference): The path to a PDB structure file, or an already initialized
+            PDBReference object.
+            chain (str): Which chain in the PDB structure to use for comparison and evaluation. If left blank the best
+            chain will be identified by aligning the query sequence from seq_alignment against the chains in
+            pdb_reference and the closest match will be selected.
+        """
+        super().__init__(query, seq_alignment, pdb_reference, chain)
