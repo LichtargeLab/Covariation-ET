70f537c528397bd385341d9dcb8ae534080cdb90
spamrick <74685167+spamrick@users.noreply.github.com>
2022-04-13 17:35:30 -0500

    Update utils.py
    minor

--- src/SupportingClasses/utils.py
+++ src/SupportingClasses/utils.py
@@ -45,8 +45,8 @@ def remove_sequences_with_ambiguous_characters(fp, out_dir, additional_chars = [
             handle.write('\n')
             handle.write(value)
             handle.write('\n')
-     print(f'{len(removed)} sequences removed for containing ambiguous characters')
-     return op
+    print(f'{len(removed)} sequences removed for containing ambiguous characters')
+    return op
 
 
 def build_mapping(alphabet, skip_letters=None):
