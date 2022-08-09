2f4be56772e01637265548f4b06a779812cf7167
spamrick <spencer.hamrick@bcm.edu>
2022-08-09 14:35:22 -0500

    Clean up C++ Script

--- src/CovET_func_py.cpp
+++ src/CovET_func_py.cpp
@@ -1,7 +1,6 @@
 //
 // Created by spencer on 7/26/22.
 //
-#include <stdlib.h>
 #include <iostream>
 #include <bits/stdc++.h>
 
@@ -19,18 +18,19 @@ double TraceGroupC(int start, int end, int group_count, int **x) {
             }
         }
     }
-    if (vec.size() == 0) {
+    if (vec.empty())
         //std::cout << "Vec.size = 0 \n";
         /* If no non concerted variant is present
          * this group has a trace of 1.
          */
         out = 1;
-    } else {
-        /* This block counts the occurrence for each non 
+    else {
+        /* This block counts the occurrence for each non
          * concerted type that present in the group.
          */
         std::map<int, double> counts;
-        int n = vec.size();
+        unsigned long n;
+        n = vec.size();
         for (int i = 0; i < n; i++) {
             counts[vec[i]]++;
         }
@@ -38,10 +38,10 @@ double TraceGroupC(int start, int end, int group_count, int **x) {
          * this group.
          */
         out = 0;
-        double freq = 0;
-        for(auto elem : counts) {
-            freq = elem.second/group_count;
-            out += freq*std::log(freq);
+        double freq;
+        for (auto elem: counts) {
+            freq = elem.second / group_count;
+            out += freq * std::log(freq);
         }
         out = exp(-out);
     }
@@ -49,14 +49,14 @@ double TraceGroupC(int start, int end, int group_count, int **x) {
 }
 extern "C" double * TraceGroupC_vec(int *start, int *end, int *group_count,
                                  int n, int **non_concerted_matrix) {
-    double *out = new double[n];
+    auto *out = new double[n];
     for (int i = 0; i < n; i++) {
         out[i] = TraceGroupC(start[i], end[i], group_count[i], non_concerted_matrix);
     }
     return out;
 }
 
-extern "C" void free_mem(double* a)
+extern "C" void free_mem(const double* a)
 {
 delete[] a;
 }
